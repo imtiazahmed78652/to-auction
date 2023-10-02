0f76ea913f664bae044d833ff1adb5637adac0e3
imtiaz ahmed <imtiazahmed@vizzweb.com>
2023-10-03 00:07:39 +0500

    first-comit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* <Navbar/> */}
       <body className={inter.className}>{children}</body>
+      {/* <Footer/> */}
     </html>
   )
 }
